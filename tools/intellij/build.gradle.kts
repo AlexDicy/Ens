d57c575606e52cad2ee3be2f15705969938efff6
Lorenzo Lapucci <halexercraft@gmail.com>
2026-07-12 12:57:03 +0200

    Add the IntelliJ plugin Gradle skeleton

---
+++ tools/intellij/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins {
+    kotlin("jvm") version "2.4.0"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
+}
+
+group = "ens"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2.4")
+        bundledPlugin("org.jetbrains.plugins.textmate")
+        plugin("com.redhat.devtools.lsp4ij:0.20.1")
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        apiVersion.set(KotlinVersion.KOTLIN_2_0)
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+            untilBuild = provider { null }
+        }
+    }
+}
+
+tasks.processResources {
+    from("../grammar/ens.tmLanguage.json") { into("textmate/ens/syntaxes") }
+    from("../grammar/language-configuration.json") { into("textmate/ens") }
+}
